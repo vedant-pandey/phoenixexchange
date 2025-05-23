174ac23c31ac0d3c3439e7759dd7ee26c7aacaa9
vedant-pandey <vedantpandey46@gmail.com>
2025-05-24 02:15:12 +0530

    fix seed order and use values from db

--- lib/phoenixexchange/exchange/ticker.ex
+++ lib/phoenixexchange/exchange/ticker.ex
@@ -1,15 +1,16 @@
 defmodule Phoenixexchange.Exchange.Ticker do
   use GenServer
   require Logger
+  import Ecto.Query
   alias Phoenix.PubSub
+  alias Phoenixexchange.Repo
+  alias Phoenixexchange.NasdaqHistoric
 
   @topic "ohlc:updates"
   @tick_interval 1000
-  # @api_key System.get_env("API_KEY") || raise " environment variable API_KEY is missing. "
-  # @hostname System.get_env("PROVIDER_HOTSNAME") || raise " environment variable PROVIDER_HOTSNAME is missing. "
 
   def start_link(_opts) do
-    GenServer.start_link(__MODULE__, %{price: 100.0, timer: nil}, name: __MODULE__)
+    GenServer.start_link(__MODULE__, %{price: 100.0, timer: nil, price_id: 1}, name: __MODULE__)
   end
 
   def init(state) do
@@ -17,36 +18,34 @@ defmodule Phoenixexchange.Exchange.Ticker do
   end
 
   def handle_info(:tick, state) do
-    new_ohlc = generate_ohlc(state.price)
+    new_ohlc = generate_ohlc(state.price_id)
 
     PubSub.broadcast(Phoenixexchange.PubSub, @topic, {:ohlc_tick, new_ohlc})
 
     timer = schedule_tick()
 
-    {:noreply, %{state | price: new_ohlc.close, timer: timer}}
+    {:noreply, %{state | price: new_ohlc.close, timer: timer, price_id: state.price_id + 1}}
   end
 
   def handle_info({:tick, initial_price}, state) do
     if state.timer, do: Process.cancel_timer(state.timer)
 
-    new_ohlc = generate_ohlc(state.price)
+    new_ohlc = generate_ohlc(state.price_id)
 
     PubSub.broadcast(Phoenixexchange.PubSub, @topic, {:ohlc_tick, new_ohlc})
 
     timer = schedule_tick()
 
-    {:noreply, %{state | price: initial_price, timer: timer}}
+    {:noreply, %{state | price: initial_price, timer: timer, price_id: state.price_id + 1}}
   end
 
-  def handle_info({:tick, initial_price, seed, volatility}, state) do
-    Logger.debug("Initial price #{initial_price} | seed #{seed} | volatility #{volatility}")
-    {:noreply, state}
-  end
+  # def handle_info({:tick, initial_price, seed, volatility}, state) do
+  #   Logger.debug("Initial price #{initial_price} | seed #{seed} | volatility #{volatility}")
+  #   {:noreply, state}
+  # end
 
   def handle_info(_, state) do
     Logger.debug("no op")
-    Logger.debug(IEx.Info.info(state))
-    Logger.debug(state)
     {:noreply, state}
   end
 
@@ -54,23 +53,20 @@ defmodule Phoenixexchange.Exchange.Ticker do
     Process.send_after(self(), :tick, @tick_interval)
   end
 
-  defp generate_ohlc(last_price) do
-    factor = :rand.normal(0, 0.01)
+  defp generate_ohlc(price_id) do
 
-    close = last_price * (1 + factor)
-
-    open = last_price
-    high = max(open, close) * (1 + :rand.uniform() * 0.005)
-    low = min(open, close) * (1 - :rand.uniform() * 0.005)
+    val = Repo.one(from d in NasdaqHistoric, where: d.id == ^price_id)
+    # val = Repo.one(from d in NasdaqHistoric, where: d.id == 1)
+    Logger.debug(val)
 
     %{
       symbol: "STOCK",
-      timestamp: DateTime.utc_now() |> DateTime.to_unix(:millisecond),
-      open: Float.round(open, 2),
-      high: Float.round(high, 2),
-      low: Float.round(low, 2),
-      close: Float.round(close, 2),
-      volume: :rand.uniform(10000)
+      timestamp: val.date,
+      open: Decimal.to_float(val.open),
+      high: Decimal.to_float(val.high),
+      low: Decimal.to_float(val.low),
+      close: Decimal.to_float(val.close),
+      volume: val.volume
     }
   end
 
